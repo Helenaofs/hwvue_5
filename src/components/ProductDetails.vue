f829e88158de2b6e31c2e1e310625214d78806a3
helenaofs <lilklinik@yandex.ru>
2023-09-09 18:44:14 +0700

    task5 added

---
+++ src/components/ProductDetails.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="product">
+        <div class="product-container">
+            <div class="product-image">
+                <img :src="product.image" :alt="korm">
+            </div>
+            <div class="product-info">
+                <h1>{{ product.name }}</h1>
+                <h2 v-if="product.available">Available</h2>
+                <h2 v-else>Out of Stock</h2>
+                <p>Price: {{ formattedPrice }}</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ProductDetails',
+
+    data() {
+        return {
+            product: {
+                name: 'kitekat',
+                price: '99.99',
+                available: true,
+                image: require('../assets/korm.jpg'),
+                alt: 'shirt',
+            },
+        }
+    },
+
+    computed: {
+        formattedPrice: function () {
+            return `$${this.product.price}`;
+        }
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
